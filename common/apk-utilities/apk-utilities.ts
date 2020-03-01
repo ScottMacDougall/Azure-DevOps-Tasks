5430e68989a15fcb9982f69cb1822ff5aaff3318
Scott MacDougall <scott56@gmail.com>
2020-03-01 16:32:49 -0500

    removed embedded apktool

--- common/apk-utilities/apk-utilities.ts
+++ common/apk-utilities/apk-utilities.ts
@@ -2,12 +2,11 @@ import tl = require('azure-pipelines-task-lib/task');
 import tr = require('azure-pipelines-task-lib/toolrunner');
 import path = require('path');
 
+import http = require('https');
+import fs = require('fs');
+
 export async function unpackApk(apkPath: string, outputPath: string, apkToolArgs: string | undefined) {
-    // Find the absolute apk file path
     let apkOutputPath: string = path.join(outputPath, "output");
-    // if (!tl.exist(apkOutputPath)) {
-    //     tl.mkdirP(apkOutputPath);
-    // }
     let bash: tr.ToolRunner = tl.tool(tl.which("java", true));
     let options = <tr.IExecOptions>{
         cwd: __dirname,
@@ -15,26 +14,40 @@ export async function unpackApk(apkPath: string, outputPath: string, apkToolArgs
         ignoreReturnCode: true,
     };
 
-    let apktool = "apktool.jar";
-    bash.arg("-jar");
-    bash.arg(path.join(__dirname, apktool));
-    bash.argIf(apkToolArgs != undefined, apkToolArgs);
-    bash.arg("d");
-    bash.arg(apkPath);
-    bash.arg("-o");
-    bash.arg(apkOutputPath);
-    let unpackResult = await bash.exec(options);
-    if (unpackResult != 0) {
-        throw new Error("Unable to unpack apk");
-    }
+    var download = function (url: string, dest: string) {
+        var file = fs.createWriteStream(dest);
+        var request = http.get(url, function (response) {
+            if (response.statusCode == 302 && response.headers.location) {
+                download(response.headers.location, dest);
+            } else {
+                response.pipe(file).on("finish", async function () {
+                    let apktool = "apktool.jar";
+                    bash.arg("-jar");
+                    bash.arg(path.join(__dirname, apktool));
+                    bash.argIf(apkToolArgs != undefined, apkToolArgs);
+                    bash.arg("d");
+                    bash.arg(apkPath);
+                    bash.arg("-o");
+                    bash.arg(apkOutputPath);
+                    let unpackResult = await bash.exec(options);
+                    if (unpackResult != 0) {
+                        throw new Error("Unable to unpack apk");
+                    }
 
-    tl.setVariable('UNPACKED_APK_PATH', apkOutputPath);
-    console.log("UNPACKED_APK_PATH: " + tl.getVariable('UNPACKED_APK_PATH'));
+                    tl.setVariable('UNPACKED_APK_PATH', apkOutputPath);
+                    console.log("UNPACKED_APK_PATH: " + tl.getVariable('UNPACKED_APK_PATH'));
 
-    tl.setVariable('UNPACKED_APK_NAME', path.basename(apkPath));
-    console.log("UNPACKED_APK_NAME: " + tl.getVariable('UNPACKED_APK_NAME'));
+                    tl.setVariable('UNPACKED_APK_NAME', path.basename(apkPath));
+                    console.log("UNPACKED_APK_NAME: " + tl.getVariable('UNPACKED_APK_NAME'));
 
-    return apkOutputPath;
+                    return apkOutputPath;
+                });
+            };
+        }).on('error', function (err) {
+            throw Error("Unable to download Apktool");
+        });
+    };
+    download("https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.4.1.jar", path.join(__dirname, "apktool.jar"));
 
 }
 
